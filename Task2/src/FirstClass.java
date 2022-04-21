ddcbf9ff0a9539e62ff8e5c534773f4f5aa72fa7
SinenkoA <sinenko.alexey66@gmail.com>
2022-04-21 18:22:16 +0300

    create FirstClass and added unit name+addres

---
+++ Task2/src/FirstClass.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What is your name?");
+        String username = scan.nextLine();
+        System.out.print("Where do you live, " + username + "?");
+        String addres = scan.nextLine();
+        System.out.print(username + " " + addres);
+
+    }
+    
+}
