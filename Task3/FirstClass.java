8551a85a63efcee35f26a7babd218836f47b1d08
SinenkoA <sinenko.alexey66@gmail.com>
2022-04-22 00:38:41 +0300

    created calculator in Task3

--- Task3/FirstClass.java
+++ Task3/FirstClass.java
@@ -1,2 +1,49 @@
+import java.util.Scanner;
+
 public class FirstClass {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("input first value:");
+        double val1 = scan.nextDouble();
+
+        System.out.print(" input operation +,-,*,/,%. ");
+        String oper = scan.nextLine();
+        oper = scan.nextLine();
+
+        System.out.print(" input second value ");
+        double val2 = scan.nextDouble();
+
+        double res;
+
+        switch (oper) {
+            case "+":
+                res = val1 + val2;
+                System.out.println("Result: " + res);
+                break;
+            case "-":
+                res = val1 - val2;
+                System.out.println("Result: " + res);
+                break;
+            case "*":
+                res = val1 * val2;
+                System.out.println("Result: " + res);
+                break;
+            case "%":
+                res = val1 % val2;
+                System.out.println("Result: " + res);
+                break;
+            case "/":
+                if (val2 == 0) System.out.println("Error");
+                else {
+                    res = val1 / val2;
+                    System.out.println("Result: " + res);
+                }
+                break;
+            default:
+                System.out.println("unsupported operation");
+
+        }
+    }
+
 }
