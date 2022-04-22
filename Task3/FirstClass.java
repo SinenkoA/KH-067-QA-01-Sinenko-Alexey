ba98f6a0253dbf6912eecaf5429eabdac15b098d
SinenkoA <sinenko.alexey66@gmail.com>
2022-04-22 19:14:03 +0300

    changed the validation

--- Task3/FirstClass.java
+++ Task3/FirstClass.java
@@ -11,39 +11,44 @@ public class FirstClass {
         String oper = scan.nextLine();
         oper = scan.nextLine();
 
-        System.out.print(" input second value ");
-        double val2 = scan.nextDouble();
+        if (oper.equals("+") || oper.equals("-") || oper.equals("*") || oper.equals("/") || oper.equals("%")) {
+            System.out.print(" input second value ");
+            double val2 = scan.nextDouble();
 
-        double res;
+            double res;
 
-        switch (oper) {
-            case "+":
-                res = val1 + val2;
-                System.out.println("Result: " + res);
-                break;
-            case "-":
-                res = val1 - val2;
-                System.out.println("Result: " + res);
-                break;
-            case "*":
-                res = val1 * val2;
-                System.out.println("Result: " + res);
-                break;
-            case "%":
-                res = val1 % val2;
-                System.out.println("Result: " + res);
-                break;
-            case "/":
-                if (val2 == 0) System.out.println("Error");
-                else {
-                    res = val1 / val2;
+            switch (oper) {
+                case "+":
+                    res = val1 + val2;
                     System.out.println("Result: " + res);
-                }
-                break;
-            default:
-                System.out.println("unsupported operation");
+                    break;
+                case "-":
+                    res = val1 - val2;
+                    System.out.println("Result: " + res);
+                    break;
+                case "*":
+                    res = val1 * val2;
+                    System.out.println("Result: " + res);
+                    break;
+                case "/":
+                    if (val2 == 0) System.out.println("Error");
+                    else {
+                        res = val1 / val2;
+                        System.out.println("Result: " + res);
+                    }
+                    break;
+                case "%":
+                    res = val1 % val2;
+                    System.out.println("Result: " + res);
+                    break;
+                default:
+                    System.out.println("unsupported operation");
+            }
 
+        } else {
+            System.out.println("unsupported operation");
         }
+
     }
 
 }
