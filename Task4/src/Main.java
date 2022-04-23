5d1a54047c96d7d9ed5e175cfbf9305e7ac3951e
SinenkoA <sinenko.alexey66@gmail.com>
2022-04-23 21:06:33 +0300

    started writing functions

--- Task4/src/Main.java
+++ Task4/src/Main.java
@@ -7,14 +7,14 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter at least three words ");
         String a = scan.nextLine();
-        String[] arr = a.split(", ");
+        String[] array = a.split(", ");
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < array.length; i++) {
 
             int e = 1;
 
             while (e == 1) {
-                System.out.println(arr[i]);
+                System.out.println(array[i]);
                 System.out.println("select a filter:");
                 System.out.println("1-maximum lenght");
                 System.out.println("2-start with");
@@ -26,20 +26,34 @@ public class Main {
 
                 switch (val) {
                     case 1:
+                        System.out.println("Enter maximum length? ");
+                        int lenght = scan.nextInt();
 
-                        System.out.println("1");
+                        for (; i < array.length; i++) {
+
+                        }
+                        System.out.println(array[lenght]);
                         break;
                     case 2:
 
-                        System.out.println("2");
+                        System.out.println("Enter first letter? ");
+                        String first = scan.nextLine();
+                        System.out.println(first);
+
                         break;
                     case 3:
 
-                        System.out.println("3");
+                        System.out.println("Enter last letter? ");
+                        String last = scan.nextLine();
+                        System.out.println(last);
+
                         break;
                     case 4:
 
-                        System.out.println("4");
+                        System.out.println("Enter part words? ");
+                        String part = scan.nextLine();
+                        System.out.println(part);
+
                         break;
                     case 0:
 
